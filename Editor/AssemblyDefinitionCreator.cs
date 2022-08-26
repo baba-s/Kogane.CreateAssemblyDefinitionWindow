f2dd8d8e5cd5c177507cc3ec40fa51c735bb10b2
baba-s <shotababa1206@gmail.com>
2022-08-26 21:38:11 +0900

    update

--- Editor/AssemblyDefinitionCreator.cs
+++ Editor/AssemblyDefinitionCreator.cs
@@ -6,98 +6,116 @@ using UnityEngine;
 
 namespace Kogane.Internal
 {
-	internal sealed class AssemblyDefinitionCreator : EditorWindow
-	{
-		private const float WINDOW_HEIGHT = 164;
-		private const float LABEL_WIDTH   = 144;
+    internal sealed class AssemblyDefinitionCreator : EditorWindow
+    {
+        private const float WINDOW_HEIGHT = 164;
+        private const float LABEL_WIDTH   = 144;
 
-		private string m_directory          = string.Empty;
-		private string m_name               = "NewAssembly";
-		private bool   m_isEditor           = false;
-		private bool   m_allowUnsafeCode    = false;
-		private bool   m_overrideReferences = false;
-		private bool   m_noEngineReferences = false;
-		private bool   m_autoReferenced     = true;
+        private string m_directory = string.Empty;
+        private string m_name      = "NewAssembly";
+        private bool   m_isEditor;
+        private bool   m_allowUnsafeCode;
+        private bool   m_overrideReferences;
+        private bool   m_noEngineReferences;
+        private bool   m_autoReferenced = true;
+        private bool   m_isInitialized;
 
-		[MenuItem( "Assets/Open Assembly Definition Creator" )]
-		private static void Open()
-		{
-			var asset     = Selection.activeObject;
-			var assetPath = AssetDatabase.GetAssetPath( asset );
-			var directory = string.IsNullOrWhiteSpace( assetPath ) ? "Assets" : assetPath;
+        [MenuItem( "Assets/Kogane/Open Assembly Definition Creator" )]
+        private static void Open()
+        {
+            var asset     = Selection.activeObject;
+            var assetPath = AssetDatabase.GetAssetPath( asset );
+            var directory = string.IsNullOrWhiteSpace( assetPath ) ? "Assets" : assetPath;
 
-			if ( !AssetDatabase.IsValidFolder( directory ) )
-			{
-				directory = Path
-						.GetDirectoryName( directory )
-						?.Replace( "\\", "/" )
-					;
-			}
+            if ( !AssetDatabase.IsValidFolder( directory ) )
+            {
+                directory = Path
+                        .GetDirectoryName( directory )
+                        ?.Replace( "\\", "/" )
+                    ;
+            }
 
-			var window = GetWindow<AssemblyDefinitionCreator>
-			(
-				utility: true,
-				title: "Assembly Definition Creator",
-				focus: true
-			);
+            var window = GetWindow<AssemblyDefinitionCreator>
+            (
+                utility: true,
+                title: "Assembly Definition Creator",
+                focus: true
+            );
 
-			window.m_directory = directory;
-			window.m_isEditor  = directory.Split( '/' ).Any( x => x == "Editor" );
+            window.m_directory = directory;
+            window.m_isEditor  = directory.Split( '/' ).Any( x => x == "Editor" );
 
-			var minSize = window.minSize;
-			var maxSize = window.maxSize;
+            var minSize = window.minSize;
+            var maxSize = window.maxSize;
 
-			minSize.y = WINDOW_HEIGHT;
-			maxSize.y = WINDOW_HEIGHT;
+            minSize.y = WINDOW_HEIGHT;
+            maxSize.y = WINDOW_HEIGHT;
 
-			window.minSize = minSize;
-			window.maxSize = maxSize;
-		}
+            window.minSize = minSize;
+            window.maxSize = maxSize;
+        }
 
-		private void OnGUI()
-		{
-			var oldLabelWidth = EditorGUIUtility.labelWidth;
-			EditorGUIUtility.labelWidth = LABEL_WIDTH;
+        private void OnGUI()
+        {
+            var current = Event.current;
 
-			EditorGUILayout.LabelField( "Path", m_directory );
+            if ( current.keyCode == KeyCode.Return )
+            {
+                OnCreate();
+                current.Use();
+                return;
+            }
 
-			m_name               = EditorGUILayout.TextField( "Name", m_name );
-			m_allowUnsafeCode    = EditorGUILayout.Toggle( "Allow 'unsafe' Code", m_allowUnsafeCode );
-			m_autoReferenced     = EditorGUILayout.Toggle( "Auto Referenced", m_autoReferenced );
-			m_overrideReferences = EditorGUILayout.Toggle( "Override References", m_overrideReferences );
-			m_noEngineReferences = EditorGUILayout.Toggle( "No Engine References", m_noEngineReferences );
-			m_isEditor           = EditorGUILayout.Toggle( "Is Editor", m_isEditor );
+            var oldLabelWidth = EditorGUIUtility.labelWidth;
+            EditorGUIUtility.labelWidth = LABEL_WIDTH;
 
-			EditorGUIUtility.labelWidth = oldLabelWidth;
+            EditorGUILayout.LabelField( "Path", m_directory );
 
-			using ( new EditorGUI.DisabledScope( string.IsNullOrWhiteSpace( m_name ) ) )
-			{
-				if ( GUILayout.Button( "Create" ) )
-				{
-					OnCreate();
-				}
-			}
-		}
+            GUI.SetNextControlName( "Name" );
+            m_name               = EditorGUILayout.TextField( "Name", m_name );
+            m_allowUnsafeCode    = EditorGUILayout.Toggle( "Allow 'unsafe' Code", m_allowUnsafeCode );
+            m_autoReferenced     = EditorGUILayout.Toggle( "Auto Referenced", m_autoReferenced );
+            m_overrideReferences = EditorGUILayout.Toggle( "Override References", m_overrideReferences );
+            m_noEngineReferences = EditorGUILayout.Toggle( "No Engine References", m_noEngineReferences );
+            m_isEditor           = EditorGUILayout.Toggle( "Is Editor", m_isEditor );
 
-		private void OnCreate()
-		{
-			var path       = $"{m_directory}/{m_name}.asmdef";
-			var uniquePath = AssetDatabase.GenerateUniqueAssetPath( path );
+            EditorGUIUtility.labelWidth = oldLabelWidth;
 
-			var data = new AssemblyDefinitionJson
-			{
-				name             = Path.GetFileNameWithoutExtension( uniquePath ),
-				includePlatforms = m_isEditor ? new[] { "Editor" } : new string[0],
-				autoReferenced   = m_autoReferenced,
-			};
+            using ( new EditorGUI.DisabledScope( string.IsNullOrWhiteSpace( m_name ) ) )
+            {
+                if ( GUILayout.Button( "Create" ) )
+                {
+                    OnCreate();
+                }
+            }
 
-			var json = JsonUtility.ToJson( data, true );
+            if ( !m_isInitialized )
+            {
+                m_isInitialized = true;
 
-			File.WriteAllText( uniquePath, json, Encoding.UTF8 );
+                EditorGUI.FocusTextInControl( "Name" );
+            }
+        }
 
-			AssetDatabase.Refresh();
+        private void OnCreate()
+        {
+            var path       = $"{m_directory}/{m_name}.asmdef";
+            var uniquePath = AssetDatabase.GenerateUniqueAssetPath( path );
 
-			Close();
-		}
-	}
+            var data = new AssemblyDefinitionJson
+            {
+                name             = Path.GetFileNameWithoutExtension( uniquePath ),
+                includePlatforms = m_isEditor ? new[] { "Editor" } : new string[ 0 ],
+                autoReferenced   = m_autoReferenced,
+            };
+
+            var json = JsonUtility.ToJson( data, true );
+
+            File.WriteAllText( uniquePath, json, Encoding.UTF8 );
+
+            AssetDatabase.Refresh();
+
+            Close();
+        }
+    }
 }
--- Editor/AssemblyDefinitionJson.cs
+++ Editor/AssemblyDefinitionJson.cs
@@ -2,19 +2,19 @@
 
 namespace Kogane.Internal
 {
-	[Serializable]
-	internal sealed class AssemblyDefinitionJson
-	{
-		public string   name                  = string.Empty;
-		public string[] references            = new string[0];
-		public string[] includePlatforms      = new string[0];
-		public string[] excludePlatforms      = new string[0];
-		public bool     allowUnsafeCode       = false;
-		public bool     overrideReferences    = false;
-		public string[] precompiledReferences = new string[0];
-		public bool     autoReferenced        = false;
-		public string[] defineConstraints     = new string[0];
-		public string[] versionDefines        = new string[0];
-		public bool     noEngineReferences    = false;
-	}
+    [Serializable]
+    internal sealed class AssemblyDefinitionJson
+    {
+        public string   name             = string.Empty;
+        public string[] references       = new string[ 0 ];
+        public string[] includePlatforms = new string[ 0 ];
+        public string[] excludePlatforms = new string[ 0 ];
+        public bool     allowUnsafeCode;
+        public bool     overrideReferences;
+        public string[] precompiledReferences = new string[ 0 ];
+        public bool     autoReferenced;
+        public string[] defineConstraints = new string[ 0 ];
+        public string[] versionDefines    = new string[ 0 ];
+        public bool     noEngineReferences;
+    }
 }
